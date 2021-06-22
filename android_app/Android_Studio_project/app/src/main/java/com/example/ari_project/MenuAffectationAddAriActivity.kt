8b8170524e58a4d19bcbeba016b5338019b8d205
Benjamin Ramoné <benjamin.ramone@outlook.fr>
2021-06-22 05:32:49 +0200

    Fix affichage ID

--- android_app/Android_Studio_project/app/src/main/java/com/example/ari_project/MenuAffectationAddAriActivity.kt
+++ android_app/Android_Studio_project/app/src/main/java/com/example/ari_project/MenuAffectationAddAriActivity.kt
@@ -1,12 +1,14 @@
 package com.example.ari_project
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ari_project.databinding.MenuAffectationAddAriBinding
 
 class MenuAffectationAddAriActivity : AppCompatActivity(){
 
     private lateinit var affectAdd : MenuAffectationAddAriBinding
+    private lateinit var textViewID : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +16,12 @@ class MenuAffectationAddAriActivity : AppCompatActivity(){
         affectAdd = MenuAffectationAddAriBinding.inflate(layoutInflater)
         setContentView(affectAdd.root)
 
+        textViewID = findViewById(R.id.text_id_affect_add)
+
         //Scan ID
         val scanID = intent?.extras?.getString("id").toString()//Ligne qui extrait l'id de l'intent.
+
+        textViewID.text = scanID
 
         title = getString(R.string.ID_equipe) + " " + scanID //titre qui contient l'ID
     }
