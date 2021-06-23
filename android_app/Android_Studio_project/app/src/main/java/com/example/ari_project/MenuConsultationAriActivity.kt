67329c5f77183dffdfb62b24b27e280c629a065d
Carrybooo <team.magikmystikfr@gmail.com>
2021-06-23 16:31:51 +0200

    modif vue consultation + ajout fonctions parsing de tous les historiques

--- android_app/Android_Studio_project/app/src/main/java/com/example/ari_project/MenuConsultationAriActivity.kt
+++ android_app/Android_Studio_project/app/src/main/java/com/example/ari_project/MenuConsultationAriActivity.kt
@@ -20,12 +20,17 @@ import java.net.URL
 class MenuConsultationAriActivity : AppCompatActivity() {
 
     private lateinit var consultationAriBinding: MenuConsultationAriBinding
-    private lateinit var etat_gonflage : TextView
-    private lateinit var lieu_stock : TextView
+    private lateinit var etatGonflage : TextView
+    private lateinit var lieuStock : TextView
 
-    private lateinit var buttonPull : Button
-    private var jsonString = "<JSON_String>"
-    private val url = URL("http://ari.juliendrieu.fr/api/ari/liste_ari.php")
+    private val urlAriList = URL("http://ari.juliendrieu.fr/api/ari/liste_ari.php")
+    private var jsonAriList = "<JSON_String>"
+    private val urlHistoriqueGonflages = URL("http://ari.juliendrieu.fr/api/ari/liste_ari.php")
+    private var jsonHistoriqueGonflages = "<JSON_String>"
+    private val urlHistoriqueControles = URL("http://ari.juliendrieu.fr/api/ari/liste_ari.php")
+    private var jsonHistoriqueControles  = "<JSON_String>"
+    private val urlHistoriqueReparations  = URL("http://ari.juliendrieu.fr/api/ari/liste_ari.php")
+    private var jsonHistoriqueReparations = "<JSON_String>"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,8 +41,8 @@ class MenuConsultationAriActivity : AppCompatActivity() {
         consultationAriBinding = MenuConsultationAriBinding.inflate(layoutInflater)
         setContentView(consultationAriBinding.root)
 
-        etat_gonflage = findViewById(R.id.text_etat_gonflage_value)
-        lieu_stock = findViewById(R.id.text_lieu_de_stock_value)
+        etatGonflage = findViewById(R.id.text_etat_gonflage_value)
+        lieuStock = findViewById(R.id.text_lieu_de_stock_value)
         val accueil = findViewById<Button>(R.id.home_button)
 
         title = getString(R.string.ID_equipe_consultation) + " " + scanID //titre qui contient l'ID
@@ -50,29 +55,30 @@ class MenuConsultationAriActivity : AppCompatActivity() {
         // io dispatcher pour les opérations réseau
         lifecycleScope.launch(Dispatchers.IO){
             //jsonString = url.getString().toString()
-            jsonString = url.readText()
+            jsonAriList = urlAriList.readText()
 
 
             // default dispatcher pour le parsing, gros travail CPU
             withContext(Dispatchers.Default){
                 //on parse le JSON récupéré pour récupérer les infos liées au scanID
-                val ari = parseJson(jsonString, scanID)
+                val ari = getAriInfo(jsonAriList, scanID)
 
                 // main dispatcher pour les interactions avec l'UI
                 withContext(Dispatchers.Main) {
-                    etat_gonflage.text = when(ari?.etat_gonflage){
+                    etatGonflage.text = when(ari?.etat_gonflage){
                         null -> "ID non trouvé"
                         1 -> "Gonflé"
                         else -> "Non gonflé/Défectueux"
                     }
-                    lieu_stock.text = when(ari?.lieu_stock){
+                    lieuStock.text = when(ari?.lieu_stock){
                         null -> "ID non trouvé"
-                        else -> ari?.lieu_stock
+                        else -> ari.lieu_stock
                     }
+
                 }
             }
         }
-        println("ENDMARKER-----------------------------------MenuConsultationAriActivity----OnCreate")
+        println("ENDMARKER------------------MenuConsultationAriActivity------------------OnCreate")
     }//ONCREATE
 
 
@@ -80,12 +86,16 @@ class MenuConsultationAriActivity : AppCompatActivity() {
 
     data class ARI(
         var id:String,
-        val etat_gonflage:Int,
-        val lieu_stock:String
+        var etat_gonflage:Int,
+        var lieu_stock:String,
+        var historique_gonflages: List<String>?,
+        var historique_controles: List<String>?,
+        var historique_reparations: List<String>?
     )
 
-    private fun parseJson(data:String, id:String):ARI?{
-        var element = ARI("",0,"")
+    private fun getAriInfo(data:String, id:String):ARI?{
+        var element = ARI("",0,"",
+            null,null, null)
         try {
             val array = JSONObject(data).getJSONArray("ari_list")
             for(i in 0 until array.length()){
@@ -93,7 +103,8 @@ class MenuConsultationAriActivity : AppCompatActivity() {
                 if(obj.getString("id")==id) {
                     val etatGonflage = obj.getInt("etat_gonflage")
                     val lieuStock = obj.getString("lieu_stock")
-                    element = ARI(id, etatGonflage, lieuStock)
+                    element = ARI(id, etatGonflage, lieuStock,
+                        null, null, null)
                 }
             }
         }catch (e:JSONException){
@@ -101,7 +112,63 @@ class MenuConsultationAriActivity : AppCompatActivity() {
         }
         return if(element.id != "") element
         else null
-    }//PARSEJSON
+    }//GETARIINFO
 
+
+    private fun getHistoriqueGonflages(data:String, id:String):List<String>?{
+        val list = mutableListOf<String>()
+        try {
+            val array = JSONObject(data).getJSONArray("ari_list")
+            for(i in 0 until array.length()){
+                val obj = JSONObject(array[i].toString())
+                if(obj.getString("id")==id) {
+                    val date = obj.getString("date")
+                    list.add(date)
+                }
+            }
+        }catch (e:JSONException){
+            Log.d("Exception", e.toString())
+        }
+        return if(list != null) list
+        else null
+    }//getHistoriqueGonflages
+
+
+    private fun getHistoriqueControles(data:String, id:String):List<String>?{
+        val list = mutableListOf<String>()
+        try {
+            val array = JSONObject(data).getJSONArray("ari_list")
+            for(i in 0 until array.length()){
+                val obj = JSONObject(array[i].toString())
+                if(obj.getString("id")==id) {
+                    val date = obj.getString("date")
+                    list.add(date)
+                }
+            }
+        }catch (e:JSONException){
+            Log.d("Exception", e.toString())
+        }
+        return if(list != null) list
+        else null
+    }//getHistoriqueControles
+
+
+    private fun getHistoriqueReparations(data:String, id:String):List<String>?{
+        val list = mutableListOf<String>()
+        try {
+            val array = JSONObject(data).getJSONArray("ari_list")
+            for(i in 0 until array.length()){
+                val obj = JSONObject(array[i].toString())
+                if(obj.getString("id")==id) {
+                    val date = obj.getString("date")
+                    list.add(date)
+                }
+            }
+        }catch (e:JSONException){
+            Log.d("Exception", e.toString())
+        }
+        return if(list != null) list
+        else null
+    }//getHistoriqueReparations
 
 }//ENBRACKET
