0ee4bceee03624cab82662e3e14ad5e5342cecbd
Alios96 <ali.joua@etudiant.univ-rennes1.fr>
2021-06-24 19:04:14 +0200

    Update reglages
    Ajout des methodes Get

--- pc_app/api/reglages/ajouter.php
+++ pc_app/api/reglages/ajouter.php
@@ -6,7 +6,13 @@ if (isset($_POST['id']) && isset($_POST['etat_gonflage']) && isset($_POST['lieu_
     $insert1 = $bdd->prepare("INSERT INTO ari(id, etat_gonflage,lieu_stock ) VALUES (?,?,?)");
     $insert1->execute(array($_POST['id'], $_POST['etat_gonflage'], $_POST['lieu_stock']));
     echo "Ari " . $_POST['id'] . " ajouté ";
+} 
+else if (isset($_GET['id']) && isset($_GET['etat_gonflage']) && isset($_GET['lieu_stock'])) {
 
-} else {
-    echo 'Les variables du formulaire ne sont pas déclarées';
+    $insert1 = $bdd->prepare("INSERT INTO ari(id, etat_gonflage,lieu_stock ) VALUES (?,?,?)");
+    $insert1->execute(array($_GET['id'], $_GET['etat_gonflage'], $_GET['lieu_stock']));
+    echo json_encode("Ari " . $_GET['id'] . " ajouté ");
+} 
+else {
+    echo json_encode('Les variables du formulaire ne sont pas déclarées');
 }
